887891c75cdbebf24de5ea9d84a24f39314efcb0
Kenneth Attans <kennykayock@gmail.com>
2022-10-14 21:54:11 -0700

    Task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; i++)
+		for (j = k; j <= '9'; j++)
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	putchar('\n');
+	return (0);
+}
