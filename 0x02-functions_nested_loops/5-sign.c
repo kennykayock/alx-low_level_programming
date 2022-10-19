570dbdd2f32623b36812056a8be76a487b909ebe
Kenneth Attans <kennykayock@gmail.com>
2022-10-18 21:57:03 -0700

    Task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
- * Return: 1 if the number is greater than zero, 0 if the number is zero, 1 if the number is less than zero
+ * Return: 1 if the number is greater than zero,
+ * 0 if the number is zero,
+ * 1 if the number is less than zero
  */
 int print_sign(int n)
 {
