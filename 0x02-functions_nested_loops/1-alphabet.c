8d523dd7b6c2af4ac876fb9a08b8362c17ed8ee2
Kenneth Attans <kennykayock@gmail.com>
2022-10-18 20:44:54 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print alphabet - prints the alpha in lowercase
+ */
+int main(void)
+{
+	char la;
+	
+	for (la='a'; la <= 'z; la++')
+		_putchar(la);
+
+	_putchar('\n');
