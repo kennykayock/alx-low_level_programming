7271372ab8ba069b0918df98c318363c29de0fd6
Kenneth Attans <kennykayock@gmail.com>
2022-10-23 06:20:35 -0700

    Task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print fizzbuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	printf("1");
+	for (n = 2; n <= 100; n++)
+	{
+		print(" ");
+		if (n % 3 == 0)
+			printf("Fizz");
+		if (n % 5 == 0)
+			printf("Buzz");
+		if (n % 3 != 0 && n % 5 != 0)
+			printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
