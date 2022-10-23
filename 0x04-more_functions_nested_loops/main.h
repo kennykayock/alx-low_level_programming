ebe7a57babd2201899b27bd7d84f6e63e6d8b8b0
Kenneth Attans <kennykayock@gmail.com>
2022-10-23 04:36:50 -0700

    Adding main header

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int isupper(int c);
-int isdigit(int c);
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
