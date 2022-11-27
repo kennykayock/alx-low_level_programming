72a9fe4e50ae93ca798fed3dea3b57e3101186ca
Kenneth Attans <kennykayock@gmail.com>
2022-11-27 14:08:09 -0800

    Adding header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,15 +1,18 @@
-#ifndef LISTS
-#define LISTS
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
